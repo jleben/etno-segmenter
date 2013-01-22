64a21dcfc7ccaa05a14116899226a9fc2d0891c3
Jakob Leben <jakob.leben@gmail.com>
2013-01-22 11:57:31 +0100

    modules: FourHzModulation: compile fixes

--- modules/4hz_modulation.hpp
+++ modules/4hz_modulation.hpp
@@ -2,58 +2,62 @@
 #ifndef SEGMENTER_4HZ_MODULATION_INCLUDED
 #define SEGMENTER_4HZ_MODULATION_INCLUDED
 
+#include "module.hpp"
+
 #include <cmath>
 
 namespace Segmenter {
 
-class FourHzModulation
+class FourHzModulation : public Module
 {
     std::vector<float> m_filter;
 
     std::vector< std::vector<float> > m_buf;
-    int m_buf_n;
+    int m_iBufWrite;
 
     float m_output;
 
 public:
     FourHzModulation( float sampleRate, int hopSize ):
-        m_buf_n(0),
+        m_iBufWrite(0),
         m_output(0.f)
     {
-        double dt = hopSize / (double) sampleRate;
-        int filterN = std::ceil( 0.5 / dt );
-        m_filter.reserve(filterN);
-        for (int n = 0; n < filterN; ++n)
-            m_filter.push_back( cos( 4 * 2 * PI * dt ) );
+        static const double pi = Segmenter::pi();
 
-        m_buf.resize( filterN );
+        double dt = hopSize / (double) sampleRate;
+        int nFilter = std::ceil( 0.5 / dt );
+        m_filter.reserve(nFilter);
+        for (int i = 0; i < nFilter; ++i)
+            m_filter.push_back( cos( 4 * 2 * pi * i * dt ) );
+
+        m_buf.resize( nFilter );
     }
 
     void process( const std::vector<float> & melSpectrum )
     {
-        m_buf[m_buf_n] = melSpectrum;
-        ++m_buf_n;
-        int spectrum_N = melSpectrum.size();
-        int filter_N = m_filter.size();
+        m_buf[m_iBufWrite] = melSpectrum;
+        ++m_iBufWrite;
+        int nSpectrum = melSpectrum.size();
+        int nFilter = m_filter.size();
 
         float filteredSpectrumEnergy = 0.f;
 
-        for (int spec_n = 0; spec_n < spectrum_N; ++spec_n)
+        for (int iSpec = 0; iSpec < nSpectrum; ++iSpec)
         {
             float filteredBin = 0.f;
             float totalBinEnergy = 0.f;
             // process 2 parts of the circular buffer:
-            int n_f, n_b;
-            for( n_b = m_buf_n, n_f = 0; n_b < N; ++n_b, ++n_f )
+            int iFilter, iBuf;
+            for( iBuf = m_iBufWrite, iFilter = 0; iBuf < nFilter; ++iBuf, ++iFilter )
             {
-                float x = m_buf_n[n][spec_n];
-                filteredBin += x * m_filter[n_f];
+                float x = m_buf[iBuf][iSpec];
+                filteredBin += x * m_filter[iFilter];
                 totalBinEnergy += x;
             }
-            for( n_b = 0; n_b < m_buf_n; ++n_b, ++n_f )
+            for( iBuf = 0; iBuf < m_iBufWrite; ++iBuf, ++iFilter )
             {
-                float x = m_buf_n[n][spec_n];
-                filteredBin += x * m_filter[n_f];
+                float x = m_buf[iBuf][iSpec];
+                filteredBin += x * m_filter[iFilter];
                 totalBinEnergy += x;
             }
 
